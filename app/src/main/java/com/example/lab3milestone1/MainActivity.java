11ef66620da1e433a1ca138b9ac0b88e902ff1e5
Sam Ramakrishnan <sramakrishn8@wisc.edu>
2020-02-16 18:07:39 -0600

    Listeners

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.lab3milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnOpenDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnOpenDialog = findViewById(R.id.btn_open_dialog);
+        btnOpenDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDIalog();
+            }
+        });
+    }
+
+    private void openDIalog() {
     }
 }
